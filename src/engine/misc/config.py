56d1c2fcd339a89c0c50c3f79d946afd3362dbd0
nrbjerg <mart547n@outlook.dk>
2022-05-14 23:07:55 +0200

    Began implementing configuration

---
+++ src/engine/misc/config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import yaml
+from typing import Dict, Any
+import os
+
+
+def load_config():
+    """Load the entiere config from the current working directory."""
+    path = os.path.join(os.getcwd(), "conf.yaml")
+    with open(path, "r") as file:
+        conf = yaml.load(file, loader=yaml.FullLoader)
+
+    return conf
+
+
+config = load_config()
